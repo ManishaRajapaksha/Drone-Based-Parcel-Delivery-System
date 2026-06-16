830e9b9530ddb1424fe715fddd241c91b60a204a
ManishaRajapaksha <rajapakshamanisha527@google.com>
2026-06-16 14:06:38 +0530

    RunSystem.java Line 506 - Removed unused method parameter

--- src/RunSystem.java
+++ src/RunSystem.java
@@ -503,7 +503,7 @@ public class RunSystem extends JFrame {
     }
 
     // ------------- MAIN FUNCTION -------------
-    public static void main(String[] args) {
+    public static void main() {
         new RunSystem();
     }
 }
